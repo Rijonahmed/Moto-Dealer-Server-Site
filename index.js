ee49bdb0858d9f4e569f4440cbeb64b7d35f34ae
Rijonahmed <rijon036@gmail.com>
2022-05-07 00:21:58 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cw6id.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+
+  console.log('connected to db')
+  // perform actions on the collection object
+  // client.close();
+});
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
