5ddcdc90285d539a57799f81ff9972064ddc86fc
Rijonahmed <rijon036@gmail.com>
2022-05-07 19:20:56 +0600

    get ,post,put,delete added

--- index.js
+++ index.js
@@ -1,18 +1,102 @@
-const express = require('express')
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cw6id.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
 
-  console.log('connected to db')
-  // perform actions on the collection object
-  // client.close();
-});
+
+async function run() {
+
+  try {
+    await client.connect();
+    const wareHouseCollection = client.db("test").collection("devices");
+
+    console.log('connected to mongodb')
+
+    //get read all inventory
+    //http://localhost:5000/inventory
+    app.get('/inventory', async (req, res) => {
+      const q = req.query;
+      console.log(q);
+
+      const cursor = wareHouseCollection.find({})
+
+      const result = await cursor.toArray()
+
+
+      res.send(result)
+    })
+
+
+
+    //create add item
+    //http://localhost:5000/item
+
+    app.post("/item", async (req, res) => {
+
+      const data = req.body;
+
+      const result = await wareHouseCollection.insertOne(data)
+      res.send(result)
+    })
+
+    //update
+
+    app.put('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log('from update api', data)
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          userName: data.userName, email: data.email,
+        },
+      };
+
+      const result = await wareHouseCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result)
+      // console.log('from put method', id)
+    })
+
+    // delete
+
+    app.delete("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+
+      const result = await wareHouseCollection.deleteOne(filter);
+
+      res.send(result);
+    })
+
+  }
+  finally {
+
+  }
+
+}
+run().catch(console.dir);
+
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+
+//   console.log('connected to db')
+//   // perform actions on the collection object
+//   // client.close();
+// });
 
 
 
